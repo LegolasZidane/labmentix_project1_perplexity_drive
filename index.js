30039b49e5ddca20253a63032d98498c45f7e18c
LegolasZidane <laraibaa6@gmail.com>
2025-08-07 07:58:43 +0530

    prepping files for day1

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Will start Insha Allah!...
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
