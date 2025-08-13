1111e2bff64b87f2a00630dbf242c11ac35027ae
LegolasZidane <laraibaa6@gmail.com>
2025-08-13 06:19:12 +0530

    setting things up

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+    
+    try{
+
+        res.send("Perplexity Drive Backend Running");
+
+    } catch (err) {
+
+        next(err);
+
+    }
+
+});
 
 app.listen(port, () => {
     console.log(`Server listening at ${port}`);
