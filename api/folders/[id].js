1d3587ffbf898eff352cf9499c42d51a1351fc16
LegolasZidane <laraibaa6@gmail.com>
2025-08-17 13:43:54 +0530

    two bugs removed

--- api/folders/[id].js
+++ api/folders/[id].js
@@ -11,7 +11,7 @@ export default async function handler(req, res){
             return await getFoldersFiles(req, res);
         } else if( req.method === "PATCH"){
             return await renameFolder(req, res);
-        } else if( req.method === "DELETE ") {
+        } else if( req.method === "DELETE") {
             return await deleteFolder(req, res);
         } else {
             return res.status(405).json({ error: "Method not allowed" });
