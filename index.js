894e6e908bf3090d45aaf24822025f314de8596b
LegolasZidane <laraibaa6@gmail.com>
2025-08-18 14:10:01 +0530

    Port bug removed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import sharedFilesRoutes from './routes/sharedFiles.js';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
